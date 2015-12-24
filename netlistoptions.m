13e245442dfdb51fda2093644a4575f0af392bf4
loudnoises <benc.holmes@icloud.com>
2015-12-24 23:42:04 +0000

    Added netlistoptions class

---
+++ netlistoptions.m
@@ -0,0 +1,13 @@
+classdef netlistoptions
+    %NETLISTOPTIONS A set of options for netlist2linss
+    
+    properties
+        Ts;
+        UseValues;
+    end
+    
+    methods
+    end
+    
+end
+
